b36c8e4d4f5c612340029191ace04eb13d2b36eb
Palanichamy1986 <45284500+Palanichamy1986@users.noreply.github.com>
2026-01-09 07:55:03 +0530

    Add files via upload

---
+++ AsyncLet/AsyncLet/AsyncLetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncLetApp.swift
+//  AsyncLet
+//
+//  Created by apple on 07/01/26.
+//
+
+import SwiftUI
+
+@main
+struct AsyncLetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
